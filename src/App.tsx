8c516c235eb9bebcea24900df4620d6cdf5e30f2
tsteelematc <tsteele@madisoncollege.edu>
2023-09-25 11:25:48 -0500

    Setting up MUI...
    Installed bunch of packages (commands below). And got a button working ! ! !
    
    Learning emotion styling for the first time. I prefer Tailwind, but here we go : - O
    
    npm install @mui/material @emotion/react @emotion/styled
    
    npm install @fontsource/roboto
    
    npm install @mui/icons-material
    
     npm install @mui/material-next

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material-next/Button';
+import Add from '@mui/icons-material/Add'
+import { css } from '@emotion/react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button 
+        size="large" 
+        variant="filled"
+        color="primary"
+        startIcon={<Add />}
+        css={css`margin-top: 20px`}
+      >
+        Add Log Entry
+      </Button>
     </div>
   );
 }
